70e771c6096f6a50bbb45be74946dfd1a1e55a1c
Viktor <viktor.nedov@gmail.com>
2024-07-04 11:22:50 +0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='my_project',
+    name='Forecasting_MiniCourse_SalesForecasting_MiniCourse_Sales
+',
     version='0.1',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
